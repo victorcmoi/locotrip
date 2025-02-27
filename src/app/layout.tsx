63dfa144adbfc20d3609e0b1e4d155b2c0fed6de
Victor LE FLOCH <vilefloch@outlook.com>
2025-02-27 16:40:56 +0100

    add gg analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Balises Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-TP3ESQNTKL`}/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-TP3ESQNTKL');
+              `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
